e04d5a161585001d1d9b6d3392421e0150a5bdaa
Svyatoslav Kryukov <me@skryukov.dev>
2025-07-30 10:42:25 +0300

    * use Alba inflector

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -3,18 +3,6 @@ class ApplicationSerializer
   include Alba::Resource
   include Typelizer::DSL
 
-  class << self
-    def one(name, serializer: nil, **options)
-      options[:resource] ||= serializer || "#{name}Serializer".classify.constantize
-      super(name, **options)
-    end
-
-    def many(name, serializer: nil, **options)
-      options[:resource] ||= serializer || "#{name.to_s.singularize}Serializer".classify.constantize
-      super(name, **options)
-    end
-  end
-
   private
 
   def default_url_options = ActiveStorage::Current.url_options
