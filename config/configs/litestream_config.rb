cadb80fc990948fa081f33868dfbddcd08a14bb0
Cirdes <cirdes@linkana.com>
2025-12-02 13:56:35 -0300

    add litestream config

--- config/configs/litestream_config.rb
+++ config/configs/litestream_config.rb
@@ -2,7 +2,7 @@ class LitestreamConfig < ApplicationConfig
   # Use a custom name to not interfere with Litestream itself
   config_name :litestream_rails
 
-  attr_config :bucket, :access_key_id, :secret_access_key
+  attr_config bucket: ENV["BUCKET_NAME"], access_key_id: ENV["AWS_ACCESS_KEY_ID"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 
   def configured? = access_key_id.present? && secret_access_key.present? && bucket.present?
 end
